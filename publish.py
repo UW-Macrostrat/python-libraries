606d63a66bf132284d85288a914b03e686a32632
Daven Quinn <dev@davenquinn.com>
2022-05-25 18:40:39 -0500

    Added updated publish scripts and requirements files

--- publish.py
+++ publish.py
@@ -13,34 +13,47 @@ setup_stderr_logs("macrostrat")
 
 environ["POETRY_VIRTUALENVS_CREATE"] = "False"
 
-def process_module(module):
-    fp = relative_path(__file__, module)
-    print("Importing module: " + module)
-    exec("import %s" % module)
-
+def process_module(fp):
     chdir(fp)
+    cmd("poetry lock")
     cmd("poetry export -f requirements.txt > requirements.txt", shell=True)
     cmd("poetry build")
 
-    version = get_version()
-    if package_exists(module, version):
-        print(f"{module} {version} already exists on PyPI")
+
+def publish_module(fp):
+    chdir(fp)
+    pkg = get_package_data()
+    if package_exists(pkg):
         return
+
     cmd("poetry publish")
     
 
-def get_version():
+def get_package_data():
     with open("pyproject.toml", "r") as f:
         data = load(f)
-        return data["tool"]["poetry"]["version"]
+        return data["tool"]["poetry"]
 
-def package_exists(module_name, version):
-    uri = f"http://pypi.python.org/pypi/{module_name}/{version}/json"
+def package_exists(pkg):
+    name = pkg["name"]
+    version = pkg["version"]
+    vstr = f"[cyan]{name}[/cyan] ([bold]{version}[/bold])"
+    print(f"Checking if {vstr} exists on PyPI")
+    uri = f"https://pypi.python.org/pypi/{name}/{version}/json"
     response = requests.get(uri)
-    return response.status_code == 200
+    pkg_exists =  response.status_code == 200
+    if pkg_exists:
+        print(f"{vstr} already exists on PyPI")
+    else:
+        print(f"Preparing to publish {vstr}")
+    return pkg_exists
 
 # You should get a PyPI API token from https://pypi.org/account/
 # and set the environment variable POETRY_PYPI_TOKEN to it.
 if __name__ == "__main__":
-    for module in modules:
-        process_module(module)
+    module_dirs = [relative_path(__file__, module) for module in modules]
+    for fp in module_dirs:
+        process_module(fp)
+
+    for fp in module_dirs:
+        publish_module(fp)
