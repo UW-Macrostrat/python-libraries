4402acb2538ac615aa1ce58d11508b840df39f6a
Daven Quinn <code@davenquinn.com>
2024-01-18 00:32:04 -0600

    Updated module versions

--- app-frame/test_app/__init__.py
+++ app-frame/test_app/__init__.py
@@ -7,7 +7,7 @@ APP_ROOT = Path(__file__).parent
 app = Application(
     "Test App",
     restart_commands={"gateway": "caddy reload --config /etc/caddy/Caddyfile"},
-    app_module="test_app",
+    log_modules=["test_app"],
     compose_files=[APP_ROOT / "docker-compose.yaml"],
 )
 main = app.control_command()
