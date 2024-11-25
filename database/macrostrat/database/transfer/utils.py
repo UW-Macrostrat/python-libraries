39f231821c7b3237543ffa5b06603f3e4d01494b
Daven Quinn <code@davenquinn.com>
2024-11-25 06:30:11 -0600

    Added new utility functions

--- database/macrostrat/database/transfer/utils.py
+++ database/macrostrat/database/transfer/utils.py
@@ -5,9 +5,9 @@ from aiofiles.threadpool.binary import AsyncBufferedIOBase
 from rich.console import Console
 from sqlalchemy.engine import Engine
 from sqlalchemy.engine.url import URL
-from sqlalchemy_utils import create_database, database_exists
+from sqlalchemy_utils import create_database, database_exists, drop_database
 
-from macrostrat.utils import get_logger
+from macrostrat.utils import get_logger, ApplicationError
 
 console = Console()
 
@@ -19,37 +19,60 @@ def _docker_local_run_args(postgres_container: str = "postgres:15"):
         "docker",
         "run",
         "-i",
-        "--rm",
         "--network",
         "host",
+        "--attach",
+        "stdin",
+        "--attach",
+        "stdout",
+        "--attach",
+        "stderr",
+        "--log-driver",
+        "none",
+        "--rm",
         postgres_container,
     ]
 
 
-def _create_database_if_not_exists(_url: URL, create=False):
+def _create_database_if_not_exists(
+    _url: URL, *, create=False, allow_exists=True, overwrite=False
+):
     database = _url.database
+    if overwrite:
+        create = True
     db_exists = database_exists(_url)
     if db_exists:
-        console.print(f"Database [bold cyan]{database}[/] already exists")
+        msg = f"Database [bold underline]{database}[/] already exists"
+        if overwrite:
+            console.print(f"{msg}, overwriting")
+            drop_database(_url)
+            db_exists = False
+        elif not allow_exists:
+            raise ApplicationError(msg, details="Use `--overwrite` to overwrite")
+        else:
+            console.print(msg)
 
     if create and not db_exists:
         console.print(f"Creating database [bold cyan]{database}[/]")
         create_database(_url)
 
     if not db_exists and not create:
-        raise ValueError(
-            f"Database [bold cyan]{database}[/] does not exist. "
-            "Use `--create` to create it."
+        raise ApplicationError(
+            f"Database [bold cyan]{database}[/] does not exist. ",
+            "Use `--create` to create it.",
         )
 
 
 def _create_command(
     engine: Engine,
     *command,
-    args=[],
+    args=None | list[str],
     prefix=None | list[str],
     container="postgres:16",
 ):
+    if args is None:
+        args = []
+
     command_prefix = prefix or _docker_local_run_args(container)
     _cmd = [*command_prefix, *command, str(engine.url), *args]
 
@@ -104,4 +127,8 @@ async def print_stdout(stream: asyncio.StreamReader):
 
 
 def raw_database_url(url: URL):
-    return str(url).replace("***", quote(url.password, safe=""))
+    """Replace the password asterisks with the actual password, in order to pass to other commands."""
+    _url = str(url)
+    if "***" not in _url or url.password is None:
+        return _url
+    return _url.replace("***", quote(url.password, safe=""))
