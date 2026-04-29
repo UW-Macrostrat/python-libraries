a65eb54ef435a1dc92ac5f4d255d292e872972d4
Daven Quinn <code@davenquinn.com>
2026-04-29 11:28:55 -0400

    All tests pass for run_sql function

--- database/macrostrat/database/query.py
+++ database/macrostrat/database/query.py
@@ -1,9 +1,10 @@
 import os
+from dataclasses import dataclass
 from enum import Enum
 from pathlib import Path
 from re import search
 from sys import stderr
-from typing import IO, Union
+from typing import Callable, Any, IO, Union
 from warnings import warn
 
 import psycopg2.sql as psql2
@@ -95,6 +96,7 @@ def summarize_statement(sql):
             if not line.startswith(i):
                 continue
             return line.split("(")[0].strip().rstrip(";").replace(" AS", "")
+    return sql.strip().split("\n")[0].strip().rstrip(";")
 
 
 def get_sql_text(sql, interpret_as_file=None, echo_file_name=True):
@@ -206,8 +208,11 @@ def _render_query(query: Union[SQL, Composed], connectable: Union[Engine, Connec
     return query.as_string(conn)
 
 
-def infer_has_server_binds(sql):
-    return "%s" in sql or search(r"%\(\w+\)s", sql)
+def infer_has_server_binds(sql) -> bool:
+    if "%s" in sql:
+        return True
+    res = search(r"%\(\w+\)s", sql)
+    return res is not None
 
 
 _default_statement_filter = lambda sql_text, params: True
@@ -232,7 +237,40 @@ def _normalize_output_args(kwargs):
     return output_mode, output_file
 
 
-def _run_sql(connectable, sql, params=None, **kwargs):
+@dataclass
+class StatementResult:
+    query: Any
+    params: Any = None
+    skip: bool = False
+    label: str | None = None
+
+    @classmethod
+    def skipped(cls, query=None, params=None) -> "StatementResult":
+        return cls(query=query, params=params, skip=True)
+
+
+@dataclass
+class StatementContext:
+    index: int
+    query: Any
+    params: Any
+    sql_text: str
+
+
+TransformFn = Callable[[StatementContext], list[StatementResult] | None]
+Connectable = Union[Engine, Connection]
+
+
+def _statement_filter_to_transform(statement_filter) -> TransformFn:
+    def transform(ctx: StatementContext) -> list[StatementResult] | None:
+        if not statement_filter(ctx.sql_text, ctx.params):
+            return [StatementResult.skipped(query=ctx.query, params=ctx.params)]
+        return None
+
+    return transform
+
+
+def _run_sql(connectable, sql, params=None, *, print_skipped=True, **kwargs):
     """
     Internal function for running a query on a SQLAlchemy connectable,
     which always returns an iterator. The wrapper function adds the option
@@ -245,17 +283,30 @@ def _run_sql(connectable, sql, params=None, **kwargs):
 
     stop_on_error = kwargs.pop("stop_on_error", False)
     raise_errors = kwargs.pop("raise_errors", False)
-    has_server_binds = kwargs.pop("has_server_binds", None)
     ensure_single_query = kwargs.pop("ensure_single_query", False)
-    statement_filter = kwargs.pop("statement_filter", _default_statement_filter)
     output_mode, output_file = _normalize_output_args(kwargs)
+    has_server_binds = kwargs.pop("has_server_binds", None)
+
+    statement_filter = kwargs.pop("statement_filter", None)
+    transform_statement: TransformFn | None = kwargs.pop("transform_statement", None)
 
     if stop_on_error:
         raise_errors = True
         warn(DeprecationWarning("stop_on_error is deprecated, use raise_errors"))
 
-    interpret_as_file = kwargs.pop("interpret_as_file", None)
+    if statement_filter is not None:
+        warn(
+            DeprecationWarning(
+                "statement_filter is deprecated, use transform_statement"
+            )
+        )
+        if transform_statement is not None:
+            raise ValueError(
+                "Cannot specify both statement_filter and transform_statement"
+            )
+        transform_statement = _statement_filter_to_transform(statement_filter)
 
+    interpret_as_file = kwargs.pop("interpret_as_file", None)
     queries = _get_queries(sql, interpret_as_file=interpret_as_file)
 
     if queries is None:
@@ -264,80 +315,116 @@ def _run_sql(connectable, sql, params=None, **kwargs):
     if ensure_single_query and len(queries) > 1:
         raise ValueError("Multiple queries passed when only one was expected")
 
-    # check if parameters is a list of the same length as the number of queries
     if not isinstance(params, list) or not len(params) == len(queries):
         params = [params] * len(queries)
 
-    for query, _params in zip(queries, params):
-        params, pre_bind_params = _split_params(_params)
-        if isinstance(query, (psql2.SQL, psql2.Composed)):
-            query = update_legacy_identifier(query)
-
-        if pre_bind_params is not None:
-            if not isinstance(query, SQL):
-                query = SQL(query)
-            # Pre-bind the parameters using psycopg
-            query = query.format(**pre_bind_params)
-
-        if isinstance(query, (SQL, Composed)):
-            query = _render_query(query, connectable)
-
-        sql_text = str(query)
-
-        if isinstance(query, str):
-            sql_text = format(query, strip_comments=True).strip()
-            if sql_text == "":
-                continue
-            # Check for server-bound parameters in sql native style. If there are none, use
-            # the SQLAlchemy text() function, otherwise use the raw query string
-            if has_server_binds is None:
-                has_server_binds = infer_has_server_binds(sql_text)
-
-        should_run = statement_filter(sql_text, params)
-
-        # Shorten summary text for printing
-        if output_mode != OutputMode.ALL:
-            sql_text = summarize_statement(sql_text)
-
-        if not should_run:
-            secho(
-                sql_text,
-                dim=True,
-                strikethrough=True,
-                file=output_file,
-            )
+    for index, (query, _params) in enumerate(zip(queries, params)):
+        _query, sql_text = _render_query_text(connectable, query, _params)
+        if sql_text == "":
             continue
 
-        try:
-            trans = connectable.begin()
-        except InvalidRequestError:
-            trans = None
-        try:
-            log.debug("Executing SQL: \n %s", query)
-            if has_server_binds:
-                conn = _get_connection(connectable)
-                res = conn.exec_driver_sql(query, params)
-            else:
-                if not isinstance(query, TextClause):
-                    query = text(query)
-                res = connectable.execute(query, params)
-            yield res
-            if trans is not None:
-                trans.commit()
-            elif hasattr(connectable, "commit"):
-                connectable.commit()
-            secho(sql_text, dim=True, file=output_file)
-        except Exception as err:
-            if trans is not None:
-                trans.rollback()
-            elif hasattr(connectable, "rollback"):
-                connectable.rollback()
-            if raise_errors or _should_raise_query_error(err):
-                raise err
+        ctx = StatementContext(
+            index=index, query=query, params=_params, sql_text=sql_text
+        )
 
-            _print_error(sql_text, err, file=output_file)
-        finally:
-            pass
+        results = transform_statement(ctx) if transform_statement is not None else None
+
+        if results is None:
+            results = [StatementResult(query=query, params=_params)]
+
+        for result in results:
+            yield from _execute_one(
+                connectable,
+                result,
+                output_file,
+                raise_errors=raise_errors,
+                output_mode=output_mode,
+                print_skipped=print_skipped,
+                has_server_binds=has_server_binds,
+            )
+
+
+def _render_query_text(connectable, query, params):
+    params, pre_bind_params = _split_params(params)
+    if isinstance(query, (psql2.SQL, psql2.Composed)):
+        query = update_legacy_identifier(query)
+
+    if pre_bind_params is not None:
+        if not isinstance(query, SQL):
+            query = SQL(query)
+        # Pre-bind the parameters using psycopg
+        query = query.format(**pre_bind_params)
+
+    if isinstance(query, (SQL, Composed)):
+        query = _render_query(query, connectable)
+
+    sql_text = str(query)
+    if isinstance(query, str):
+        sql_text = format(query, strip_comments=True).strip()
+
+    return query, sql_text
+
+
+def _execute_one(
+    connectable,
+    result: StatementResult,
+    output_file: IO,
+    *,
+    raise_errors: bool = True,
+    output_mode: OutputMode = OutputMode.SUMMARY,
+    has_server_binds: bool | None = None,
+    print_skipped: bool = True,
+):
+    params = result.params
+
+    query, sql_text = _render_query_text(connectable, result.query, params)
+    if has_server_binds is None:
+        has_server_binds = infer_has_server_binds(sql_text)
+
+    if result.label is not None:
+        display_text = result.label
+    elif output_mode != OutputMode.ALL:
+        display_text = summarize_statement(str(query))
+    else:
+        display_text = str(query)
+
+    if result.skip:
+        if print_skipped:
+            secho(display_text, dim=True, strikethrough=True, file=output_file)
+        return
+
+    try:
+        trans = connectable.begin()
+    except InvalidRequestError:
+        trans = None
+
+    try:
+        log.debug("Executing SQL: \n %s", query)
+        if has_server_binds:
+            conn = _get_connection(connectable)
+            res = conn.exec_driver_sql(query, params)
+        else:
+            if not isinstance(query, TextClause):
+                query = text(query)
+            res = connectable.execute(query, params)
+
+        yield res
+
+        if trans is not None:
+            trans.commit()
+        elif hasattr(connectable, "commit"):
+            connectable.commit()
+
+        secho(display_text, dim=True, file=output_file)
+
+    except Exception as err:
+        if trans is not None:
+            trans.rollback()
+        elif hasattr(connectable, "rollback"):
+            connectable.rollback()
+        if raise_errors or _should_raise_query_error(err):
+            raise err
+        _print_error(display_text, err, file=output_file)
 
 
 def _should_raise_query_error(err):
@@ -483,6 +570,11 @@ def run_sql(*args, **kwargs):
     statement_filter : Callable
         A function that takes a SQL statement and parameters and returns True if the statement
         should be run, and False if it should be skipped.
+    transform_statement: TransformFn | None
+        A function that takes a StatementContext and returns a list of StatementResult
+        objects, which can modify the query, parameters, and whether the statement
+        should be skipped or not. This allows for more complex logic than a simple
+        statement filter.
     """
     res = _run_sql(*args, **kwargs)
     if kwargs.pop("yield_results", False):
